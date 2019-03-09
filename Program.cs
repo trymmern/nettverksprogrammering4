66b752321094158929d94ac2659f62208af7184b
Trym Røsand Todalshaug <trymrt@gmail.com>
2019-03-09 14:43:40 +0100

    initialized dotnet

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Oving4Udp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
